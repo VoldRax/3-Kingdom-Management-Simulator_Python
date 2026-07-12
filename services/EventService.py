2644ede7b7a3cdf7800501293833c98850a30aea
VoldRax <rudravoldigoad@gmail.com>
2026-07-12 22:14:10 +0530

    Add initial structure for EventService class with event triggering methods

--- services/EventService.py
+++ services/EventService.py
@@ -1,1 +1,13 @@
+import random
+
+
 class EventService:
+
+    def __init__(self, kingdom):
+        self.kingdom = kingdom
+
+    def trigger_random_event(self):
+        pass
+
+    def apply_event(self, event):
+        pass
