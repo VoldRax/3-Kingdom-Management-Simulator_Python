1d6edc33865fa63cfefc3ff9e46ca65a9323dbf8
VoldRax <rudravoldigoad@gmail.com>
2026-07-21 06:22:07 +0530

    Add ReportService and SaveService classes with initial methods

---
+++ services/reportService.py
@@ -0,0 +1,19 @@
+class ReportService:
+    
+    def __init__(self, kingdom):
+        self.kingdom = kingdom
+
+    def kingdom_report(self):
+        pass
+
+    def economy_report(self):
+        pass
+    
+    def army_report(self):
+        pass
+
+    def population_report(self):
+        pass
+
+    def resource_report(self):
+        pass
---
+++ services/saveService.py
@@ -0,0 +1,13 @@
+class SaveService:
+
+    def __init__(self, kingdom):
+        self.kingdom = kingdom
+
+    def save(self, filename):
+        pass
+
+    def load(self):
+        pass
+
+    def autosave(self):
+        pass
